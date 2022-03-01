8b77086099a73945bf9455d964fa3cba31893330
mobluo <93568065+mobluo@users.noreply.github.com>
2022-03-01 12:09:33 +0800

    Add files via upload

--- D1/anagram.py
+++ D1/anagram.py
@@ -17,17 +17,18 @@ def anagram_judge1(s1, s2):
 def anagram_judge2(s1, s2):
     list1 = list(s1)
     list2 = list(s2)
-    list1 = list1.sort()
-    list2 = list2.sort()
+    list1.sort()                            #重要
+    list2.sort()
     i = 0
-    n = len(list1)
+    n = len(s1)
+    res = True
     # while (i < n):
     #     if list1[i] == list2[i]:
     #         i = i + 1
     #     else:
     #         res = False
     #         break
-    while (i < n) and res == True:
+    while (i < n) and res:
         if list1[i] == list2[i]:
             i = i + 1
         else:
@@ -39,8 +40,8 @@ def anagram_judge3(s1, s2):
     n = len(s1)
     pos = 0
     i =0
-    tem1 = []
-    tem2 = []
+    tem1 = [0]*26                                       #重要
+    tem2 = [0]*26
     # list1 = list(s1)
     # list2 = list(s2)
     # for l1 in list1:
@@ -57,15 +58,46 @@ def anagram_judge3(s1, s2):
         pos = ord(s2[j]) - ord('a')
         tem2[pos] = tem2[pos] + 1
 
-    while i<n and res == True:
+    res = True
+    while i<n and res:
         if tem1[i] ==tem2[i]:
             i = i+1
         else:
             res = False
     return res
+# 解法4，字典，类似解法3
+def anagram_judge4(s1, s2):
+    n = len(s1)
+    i =0
+    list1 =list(s1)
+    list2 = list(s2)
+    d1 = dict()
+    d2 = dict()
+    for l1 in list1:
+        if l1  not in d1:
+            d1[l1] = 1
+        else:
+            d1[l1] = d1[l1] +1
+    for l2 in list2:
+        if l2  not in d2:
+            d2[l2] = 1
+        else:
+            d2[l2] = d2[l2] +1
+
+    res = True
+    # for l1 in d1:
+    #     if d1[l1] == d2[l1]:
+    #         continue
+    #     else:
+    #         res = False
+    #         break
+    if d1 !=d2:
+        res =False
+
+    return  res
 
 
-# 解法4，字典
 
 
-print(anagram_judge1("pytwqhon", "teryphon"))
+# anagram_judge4("pyaatwqhon", "teryphoddn")
+print(anagram_judge4("pyweteehon", "typhoeeewn"))
