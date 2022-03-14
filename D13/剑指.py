9a0fe97ea5bcd8109769b17408b7099d84835623
mobluo <93568065+mobluo@users.noreply.github.com>
2022-03-14 18:39:59 +0800

    Add files via upload

---
+++ D13/剑指.py
@@ -0,0 +1,65 @@
+# 剑指 Offer 06. 从尾到头打印链表
+# 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        resList = []
+        while head:
+            resList.append(head.val)
+            head = head.next
+        return resList[::-1]
+
+
+
+
+# 剑指 Offer 24. 反转链表
+# 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur,pre = head,None
+        while(cur):
+            tmp = cur.next # 暂存后继节点 cur.next
+            cur.next = pre # 修改 next 引用指向
+            pre = cur      # pre 暂存 cur
+            cur = tmp      # cur 访问下一节点
+        return pre
+
+
+# 剑指 Offer 35. 复杂链表的复制
+# 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if not head: return
+        dic = {}
+        # 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+        cur = head
+        while cur:
+            dic[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        # 4. 构建新节点的 next 和 random 指向
+        while cur:
+            dic[cur].next = dic.get(cur.next)
+            dic[cur].random = dic.get(cur.random)
+            cur = cur.next
+        # 5. 返回新链表的头节点
+        return dic[head]
